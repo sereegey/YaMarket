1e1f3feb3ff799546ad81d9ab244cdd1d460d7f3
solovev <sereegey@yandex.ru>
2019-09-11 20:30:27 +0400

    Добавление в корзину с инспользованием поисковой строки.

--- src/test/java/YaMarketTest.java
+++ src/test/java/YaMarketTest.java
@@ -14,98 +14,58 @@ public class YaMarketTest {
     @Test
     public void marketTest(){
         System.setProperty("webdriwer.chrome.driver", "src/test/resources/chromedriver.exe");
+        String product = "Фитнес-браслет Xiaomi Mi Band 3 Black (XMSH05HM)";
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        //driver.get("https://market.yandex.ru/");
-        //driver.get("https://market.yandex.ru/product--braslet-xiaomi-mi-band-4-nfc/471443054?hid=10498025&rt=4&suggest_text=Xiaomi%20Mi%20Band%204%20NFC&suggest=1&suggest_type=model&suggest_reqid=85128892874921400087423712896832");
-        driver.get("https://www.eldorado.ru/cat/detail/71512688");
-        driver.findElement(By.partialLinkText("миллионов")).sendKeys("123");
-        WebElement element = driver.findElement(By.className("catalogItemDetailHd"));
-        //WebElement element = driver.findElement(By.partialLinkText("Избранное"));
-        //String prod = element.getAttribute("name");
-        //System.out.println(prod);
+        driver.manage().window().maximize();
+        driver.get("https://www.eldorado.ru");
+        //WebDriverWait wait = new WebDriverWait(driver, 10);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.xpath("//input[@placeholder = 'Поиск среди миллионов товаров']")).sendKeys(product);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='Найти']")));
+        driver.findElement(By.xpath("//*[text()='Найти']")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Добавить в корзину")));
         driver.findElement(By.linkText("Добавить в корзину")).click();
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        driver.findElement(By.linkText("Перейти в корзину")).click();
-        //wait = new WebDriverWait(driver, 5);
-        //element.sendKeys("GILLETTE");
-
-        //driver.findElement(By.id("btnSrch")).click();
-        //WebElement element = driver.findElement(By.className("btn-wishlist-link  "));
-        //element.findElement(By.cssSelector("/wishlist/index/")).click();
-        //element.sendKeys("13.3\" Ноутбук Apple MacBook Air (MQD32RU/A) серебристый");
-        //driver.findElement(By.className("search2__button")).click();
-        //String product = driver.findElement(By.className("title title_size_28 title_bold_yes")).toString();
-        //driver.findElement(By.partialLinkText("Отложить")).click();
-        //driver.findElement(By.id("ui-input-search__icon ui-input-search__icon_search")).click();
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("В корзине")));
+        driver.findElement(By.linkText("В корзине")).click();
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //driver.findElement(By.linkText("В корзине")).click();
-        //driver.findElement(By.partialLinkText("Отложенные")).click();
-        //String result = driver.findElement(By.className("snippet-card__header-link shop-history__link link i-bem link_js_inited")).toString();
-        /*if (product.equals(result)){
-            System.out.println("Всё норм");
+        String text = driver.findElement(By.xpath("//a[@href = '/cat/detail/71381975/']")).getText();
+        //System.out.println(text);
+        if (product.equals(text)){
+            System.out.println("Товар добавлен в корзину");
         }
         else {
-            System.out.println("Yt yjhv");
-        }*/
+            System.out.println("Товар не добавлен в корзину");
+        }
+        driver.findElement(By.xpath("//span[text() = 'Удалить']")).click();
         try {
-            Thread.sleep(4000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         driver.quit();
     }
-    /*WebDriver webDriver;
-    WebDriverWait webDriverWait;
-    public YaMarketTest (WebDriver driver){
-        webDriver = driver;
-        webDriverWait = new WebDriverWait(webDriver, 30);
-        driver.get("http://www.guru99.com/live-testing-project.html");
-        PageFactory.initElements(webDriver, this);
-    }
-    @FindBy(css = "a[title = 'Java']")
-    private WebElement javaTitle;
-    public void clickToJavaTitle(){
-        javaTitle.click();
-    }
-    @Test
-    public void titleTest(){
-        System.setProperty("webdriwer.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.guru99.com/live-testing-project.html");
-        Assert.assertEquals(driver.getTitle(), "Live Manual Testing Project: Online Software Testing Practice");
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 30, 500);
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("g-grid")));
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.close();
-    }
-    @Test
-    public void titleTestNew(){
-        System.setProperty("webdriwer.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.guru99.com/live-testing-project.html");
-        //driver1.findElement(By.xpath("//*(text() = 'Web')"));
-        driver.findElement(By.partialLinkText("Java")).click();
-        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.guru99.com/java-tutorial.html"));
-        driver.close();
-    }
-    @Test
-    public void mainPage(){
-
-    }*/
 }
